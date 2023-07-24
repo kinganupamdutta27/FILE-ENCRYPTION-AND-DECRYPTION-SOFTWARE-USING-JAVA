ec230c8fb022264e1d23b10efa560299f0291c66
Anupam Dutta <56832807+kinganupamdutta27@users.noreply.github.com>
2023-07-24 09:36:45 +0530

    v2.0

--- src/JavaSecureFiles/JavaSecureFiles.java
+++ src/JavaSecureFiles/JavaSecureFiles.java
@@ -1,10 +1,14 @@
 package JavaSecureFiles;
 
 import javax.swing.*;
+import java.awt.geom.RoundRectangle2D;
 import java.awt.*;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.awt.geom.*;
+import javax.swing.*;
+import javax.swing.border.*;
 
 public class JavaSecureFiles {
     public static void operate(int key) {
@@ -39,7 +43,7 @@ public class JavaSecureFiles {
         JOptionPane.showMessageDialog(null, "Step 1: Enter The Password for Encryption or Decryption \nStep 2: Select the File \n\n\n* In Case of Decryption if you provide a wrong password then that file will be damaged...");
         JFrame F = new JFrame();
         // Icon Image , Create Image Icon
-        Image icon = Toolkit.getDefaultToolkit().getImage("D:\\JavaWorks\\JavaSecureFiles\\IMG\\B1.png");
+        Image icon = Toolkit.getDefaultToolkit().getImage("IMG\\B1.png");
         F.setTitle("Encrypt and Decrypt Data Software");
         F.setSize(500, 500);
         F.setLocationRelativeTo(null);
@@ -49,9 +53,9 @@ public class JavaSecureFiles {
         Font font1 = new Font("Roboto", Font.ITALIC, 50);
         Font font3 = new Font("Arial", Font.ITALIC, 30);
         // Image
-        ImageIcon img1 = new ImageIcon("D:\\JavaWorks\\JavaSecureFiles\\IMG\\jis.png");
-        ImageIcon img2 = new ImageIcon("D:\\JavaWorks\\JavaSecureFiles\\IMG\\cse1.png");
-        ImageIcon img3 = new ImageIcon("D:\\JavaWorks\\JavaSecureFiles\\IMG\\jisgr1.png");
+        ImageIcon img1 = new ImageIcon("IMG\\jis.png");
+        ImageIcon img2 = new ImageIcon("IMG\\cse1.png");
+        ImageIcon img3 = new ImageIcon("IMG\\jisgr1.png");
         // Level
         JLabel l1 = new JLabel("");
         JLabel l2 = new JLabel("");
@@ -63,12 +67,24 @@ public class JavaSecureFiles {
         l1.setIcon(img1);
         l2.setIcon(img2);
         l3.setIcon(img3);
+
         // Create Button
-        JButton Button = new JButton(new ImageIcon("D:\\JavaWorks\\JavaSecureFiles\\IMG\\image.png"));
-        Button.setText("Open Any File");
+        // Create the first button with an image icon
+        JButton Button = new JButton(new ImageIcon("IMG\\image.png"));
         Button.setFont(font);
-        Button.setBackground(Color.BLUE);
+        Button.setBackground(new Color(59, 89, 182)); // Blue color
         Button.setForeground(Color.WHITE);
+        Button.setFocusPainted(false);
+        Button.setBorder(new RoundedBorder(40)); // Set the border with a 40-pixel radius
+        // Set the text for the first button
+        Button.setText("Select File");
+        // Set the font for the first button (assuming 'font' is defined elsewhere)
+        Button.setFont(font);
+        // Set the background color for the first button to blue
+        Button.setBackground(Color.BLUE);
+        // Set the foreground color for the first button to white
+        Button.setForeground(Color.WHITE);
+        // Create the second button with the text "Clear"
         JButton Clear = new JButton("Clear");
         // Create Text Field
         JTextField T1;
@@ -88,9 +104,7 @@ public class JavaSecureFiles {
             Button.setForeground(Color.GREEN);
         });
         // Clear Button Listener
-        Clear.addActionListener(e -> {
-            T1.setText("");
-        });
+        Clear.addActionListener(e -> T1.setText(""));
         // Layout
         F.setLayout(new FlowLayout());
         F.getContentPane().setBackground(Color.CYAN);
@@ -104,5 +118,26 @@ public class JavaSecureFiles {
         F.add(T1);
         F.add(Clear);
         F.setVisible(true);
+    }
+    static class RoundedBorder implements Border {
+        private int radius;
+
+        RoundedBorder(int radius) {
+            this.radius = radius;
+        }
+
+        public Insets getBorderInsets(Component c) {
+            return new Insets(this.radius + 1, this.radius + 1, this.radius + 2, this.radius);
+        }
+
+        public boolean isBorderOpaque() {
+            return true;
+        }
+
+        public void paintBorder(Component c, Graphics g, int x, int y, int width, int height) {
+            g.setColor(c.getForeground());
+            ((Graphics2D) g).setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+            g.drawRoundRect(x, y, width - 1, height - 1, radius, radius);
+        }
     }
 }
